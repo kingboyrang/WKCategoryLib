e527b0b403f94fec50b9fa7ae2842cd173261730
kingboyrang <wuchaorang.2008@163.com>
2021-11-13 22:28:12 +0800

    1.添回base64

---
+++ WKCategoryLib/Classes/Category/NSData+WKBase64.h
@@ -0,0 +1,42 @@
+//
+//  NSData+WKBase64.h
+//  Pods
+//
+//  Created by kingboyrang on 2021/11/13.
+//
+
+#import <AppKit/AppKit.h>
+
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSData (WKBase64)
+
+/**
+ *  @brief  字符串base64后转data
+ *
+ *  @param string 传入字符串
+ *
+ *  @return 传入字符串 base64后的data
+ */
++ (NSData *)dataWithBase64EncodedString:(NSString *)string;
+/**
+ *  @brief  NSData转string
+ *
+ *  @param wrapWidth 换行长度  76  64
+ *
+ *  @return base64后的字符串
+ */
+- (NSString *)base64EncodedStringWithWrapWidth:(NSUInteger)wrapWidth;
+/**
+ *  @brief  NSData转string 换行长度默认64
+ *
+ *  @return base64后的字符串
+ */
+- (NSString *)base64EncodedString;
+
+@end
+
+NS_ASSUME_NONNULL_END
